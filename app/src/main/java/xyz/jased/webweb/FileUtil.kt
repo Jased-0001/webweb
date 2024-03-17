83cebc5e129c100a4ff83635414e0de6eb84bf9f
Jase <jased@jased.xyz>
2024-03-16 23:06:39 -0400

    lastVisitedPage.ww works now

---
+++ app/src/main/java/xyz/jased/webweb/FileUtil.kt
@@ -0,0 +1,38 @@
+package xyz.jased.webweb
+
+import android.content.Context
+import android.util.Log
+import java.io.File
+import java.io.IOException
+
+class FileUtil {
+    companion object {
+        fun writeFile(fileName: String, data: String, applicationContext: Context): Boolean { // https://www.bezkoder.com/kotlin-write-file/
+            Log.d(null, "DEBUG: ${applicationContext.filesDir}/$fileName - $data")
+
+            try {
+                File(applicationContext.filesDir, fileName).bufferedWriter().use { fileOut ->
+                    fileOut.write(data)
+                }
+
+                return true
+            } catch (e: IOException) {
+                return false
+            }
+        }
+
+        fun readFile(fileName: String, applicationContext: Context): String? {
+            Log.d(null, "DEBUG: ${applicationContext.filesDir}/$fileName - READ")
+
+            try {
+                File(applicationContext.filesDir, fileName).bufferedReader().use { fileOut ->
+                    val fileData = fileOut.readText()
+                    Log.d(null, "DEBUG: ${applicationContext.filesDir}/$fileName - GOT $fileData")
+                    return fileData
+                }
+            } catch (e: IOException) {
+                return null
+            }
+        }
+    }
+}
--- app/src/main/java/xyz/jased/webweb/MainActivity.kt
+++ app/src/main/java/xyz/jased/webweb/MainActivity.kt
@@ -1,11 +1,9 @@
 package xyz.jased.webweb
 
 import android.app.Activity
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
 
@@ -15,39 +13,50 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
 
-        initialize(savedInstanceState);
-        initializeLogic();
+        initialize(savedInstanceState)
+        initializeLogic()
     }
 
     private fun initialize(savedInstanceState: Bundle?) {
-        val go_bttn = findViewById<Button>(R.id.go_bttn)
-        val home_bttn = findViewById<Button>(R.id.home_bttn)
-
+        val goBttn = findViewById<Button>(R.id.go_bttn)
+        val homeBttn = findViewById<Button>(R.id.home_bttn)
         val webview1 = findViewById<WebView>(R.id.webview1)
-
         val edittext1 = findViewById<EditText>(R.id.edittext1)
 
-        webview1.webViewClient = object : WebViewClient() {
-            override fun onPageStarted(_param1: WebView, _param2: String, _param3: Bitmap?) {
-                edittext1.setText(_param2)
-                super.onPageStarted(_param1, _param2, _param3)
-            }
-
-            override fun onPageFinished(_param1: WebView, _param2: String) {
-                val _url = _param2
-                super.onPageFinished(_param1, _param2)
-            }
+        // Click listener for GO button
+        goBttn?.setOnClickListener {
+            loadURL(webview1, edittext1, edittext1.text.toString())
         }
 
-        go_bttn?.setOnClickListener() {
-            webview1.loadUrl(edittext1.text.toString())
+        // Click listener for HOME button (go to google)
+        // TODO: when settings page made, make this changeable
+        homeBttn?.setOnClickListener {
+            loadURL(webview1, edittext1, "https://google.com")
         }
 
-        home_bttn?.setOnClickListener {
-            webview1.loadUrl("https://google.com/")
-            edittext1.setText("https://google.com/")
+        // Load lastVisitedPage.ww
+        val urlToLoad = FileUtil.readFile("lastVisitedPage.ww", applicationContext) // TODO: bug: opens chrome
+        if (urlToLoad != null) {
+            loadURL(webView = webview1, editText = edittext1, url = urlToLoad)
+        } else {
+            Log.i(null, "INFO: FileUtil.readFile failed, maybe lVP file wasn't found?")
         }
     }
 
     private fun initializeLogic() {}
+
+    // Other utilities for me
+    private fun loadURL(webView: WebView, editText: EditText, url: String) {
+        Log.d(null,"DEBUG: loadURL $url")
+        editText.setText(url)
+        webView.loadUrl(editText.text.toString())
+
+        FileUtil.writeFile("lastVisitedPage.ww", editText.text.toString(), applicationContext)
+    }
+    private fun error(e: Throwable) {
+        // TODO: tell user about errors (custom webpage?)
+        println("ERROR: ${e.message}")
+
+        return
+    }
 }
